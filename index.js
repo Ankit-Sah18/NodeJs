99bc4de91e0b93447806811e69fd9b989cf9ea45
Ankit Sah <101566900+Ankit-Sah-Developer@users.noreply.github.com>
2023-02-01 18:37:51 +0530

    setup of node.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express= require("express");
+
+const app= express();
+app.use(express.json());
+const port = 8081;
+const connection = "mongodb+srv://test:test@cluster0.wfuqbtw.mongodb.net/test";
+  app.get("/",async(req,res)=>{
+
+    // mongodb = mongodb+srv://test:<password>@cluster0.wfuqbtw.mongodb.net/test
+
+        res.send("this is node js...");
+  })
+app.listen(port, async()=>{
+
+    try{
+
+        await connection;
+
+           console.log("server connected");
+    }
+    catch(error){
+        console.log("connection failed");
+    }
+
+    console.log(`listing to port ${port} `);
+})
